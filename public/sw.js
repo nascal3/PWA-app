c2b59d15f77683de36d14fd5496ddda4827f2608
devlap <nascal3@gmail.com>
2018-11-30 17:38:04 +0300

    add option to check if url request is already fetched if not then fetch from cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = 'static-v13';
+var CACHE_STATIC_NAME = 'static-v14';
 var CACHE_DYNAMIC_NAME = 'dynamic-v5';
 
 self.addEventListener('install', function (event) {
@@ -41,17 +41,47 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
+// FETCH FROM CACHE THEN FROM NETWORK
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-          .then(function (cache) {
-            return fetch(event.request)
-                .then(function (res) {
-                  cache.put(event.request, res.clone());
-                  return res;
-                });
+  var url = 'https://httpbin.org/get';
+
+  if (event.request.url.indexOf(url) > 1) {
+    event.respondWith(
+        caches.open(CACHE_DYNAMIC_NAME)
+            .then(function (cache) {
+              return fetch(event.request)
+                  .then(function (res) {
+                    cache.put(event.request, res.clone());
+                    return res;
+                  });
+            })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+          .then(function (response) {
+            if (response) {
+              return response;
+            } else {
+              return fetch(event.request)
+                  .then(function (res) {
+                    return caches.open(CACHE_DYNAMIC_NAME)
+                        .then(function (cache) {
+                          cache.put(event.request.url, res.clone());
+                          return res;
+                        })
+                  })
+                  .catch(function (err) {
+                    return caches.open(CACHE_STATIC_NAME)
+                        .then(function (cache) {
+                          return cache.match('/offline.html');
+                        });
+                  });
+            }
           })
-  );
+    );
+  }
+
 });
 
 // DYNAMIC CACHING
