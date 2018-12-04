b96acde19d5bf86ef456c698a1c54ff5426d87d5
devlap <nascal3@gmail.com>
2018-12-04 17:21:53 +0300

    add loop to check if URLs are in static cache

--- public/sw.js
+++ public/sw.js
@@ -42,6 +42,15 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
+function isInArray (string, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === string) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // FETCH FROM CACHE THEN FROM NETWORK
 self.addEventListener('fetch', function (event) {
   var url = 'https://httpbin.org/get';
@@ -57,7 +66,7 @@ self.addEventListener('fetch', function (event) {
                   });
             })
     );
-  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
     event.respondWith(
       caches.match(event.request)
     );
